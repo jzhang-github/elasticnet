e241eeb7f0b30ecbf76e6d7bc4fb499d8bd7425f
jzhang <18326675997@163.com>
2024-03-20 22:26:26 +0800

    update

--- elasticnet/__init__.py
+++ elasticnet/__init__.py
@@ -5,8 +5,10 @@ Created on Wed Jul 12 10:36:45 2023
 @author: ZHANG Jun
 """
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 
 from elasticnet.ann import import_data, Training_module, CV_ML_RUN, load_and_pred
 from elasticnet.lib import HecFeatureGenerator, HecPropertyParser, HecFeatureParser, predict_formula, get_concentration_from_ase, get_models, soap_feature, high_throughput_predict, ternary_plot, get_rom
 from elasticnet.prepare_input import x_main, y_main
+
+
--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ Created on Tue Jul 11 13:22:58 2023
 from distutils.core import setup
 from setuptools import find_packages
 
+
 with open("README.md", "r") as f:
   long_description = f.read()
 
@@ -16,7 +17,7 @@ with open("README.md", "r") as f:
 #     requirements = [x.strip() for x in requirements]
 
 setup(name='elasticnet',
-      version='1.0.3',
+      version='1.0.4',
       python_requires='>=3.9',
       description='Predict the mechanical properties of multi-component transition metal carbides (MTMCs).',
       long_description=long_description,
@@ -25,11 +26,11 @@ setup(name='elasticnet',
       author_email='j.zhang@my.cityu.edu.hk',
       url='https://github.com/jzhang-github/MTMC_elastic_modulus',
       install_requires=['numpy>=1.25.0',
- 'scikit-learn>=1.2.2',
- 'tensorflow>=2.10.0',
- 'ase>=3.22.1',
- 'pandas>=1.5.3',
- 'openpyxl>=3.0'],
+ 'scikit-learn==1.2.2',
+ 'tensorflow==2.10.0',
+ 'ase==3.22.1',
+ 'pandas==1.5.3',
+ 'openpyxl==3.0'],
       license='MIT',
       packages=find_packages(),
       platforms=["all"],
